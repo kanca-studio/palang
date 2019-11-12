09f443201a61db6b164e1e9772076873c590969b
Andi N. Dirgantara <andi.n.dirgantara@gmail.com>
2019-11-12 19:55:33 +0700

    add phone number & username as identifier

--- service/user/model.go
+++ service/user/model.go
@@ -4,9 +4,11 @@ import "github.com/jinzhu/gorm"
 
 type Model struct {
 	gorm.Model
-	Name               string `gorm:"column:name"`
-	Email              string `gorm:"column:email"`
-	Password           string `gorm:"column:password"`
-	Verified           bool   `gorm:"column:verified"`
-	VerificationToken  string `gorm:"column:verification_token"`
+	Name              string `gorm:"column:name"`
+	Email             string `gorm:"column:email"`
+	PhoneNumber       string `gorm:"column:phone_number"`
+	Username          string `gorm:"column:phone_number"`
+	Password          string `gorm:"column:password"`
+	Verified          bool   `gorm:"column:verified"`
+	VerificationToken string `gorm:"column:verification_token"`
 }
